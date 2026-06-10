6c2af8e97f5781acd6e9bec7620b4024ce99646b
rajib <hasan.rajib39@gmail.com>
2026-06-10 18:15:57 +0600

    go

--- app/store/cartStore.tsx
+++ app/store/cartStore.tsx
@@ -1,11 +1,19 @@
 import { create } from 'zustand'
 import { CartStoreActionsType, CartStoreStateType } from '../types'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-const useCartStore = create<CartStoreStateType & CartStoreActionsType>()((set) => ({
-  cart: [],
-  addToCart: (product) => set((state) => ({cart: [...state.cart,product]})),
-  removeFromCart: (product) => set((state) => ({cart: state.cart.filter(p=>p.id !== product.id)})),
-  clearCart: () => set({cart: [] }),
-}))
+const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
+    persist(
+        (set) => ({
+            cart: [],
+            addToCart: (product) => set((state) => ({cart: [...state.cart,product]})),
+            removeFromCart: (product) => set((state) => ({cart: state.cart.filter(p=>p.id !== product.id)})),
+            clearCart: () => set({cart: [] }),
+        }),
+        {
+            name: "cart",
+            storage: createJSONStorage(() => localStorage)
+        })
+    )
 
 export default useCartStore

45e25c7c91a4d97c6381375a293c7bcc84550018
rajib <hasan.rajib39@gmail.com>
2026-06-14 21:11:16 +0600

    batter call sall

--- app/components/ProductInteraction.tsx
+++ app/components/ProductInteraction.tsx
@@ -1,6 +1,8 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { ProductType } from "../types";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { FaMinus, FaPlus } from "react-icons/fa";
 
 const ProductInteraction = ({
   product,
@@ -11,6 +13,28 @@ const ProductInteraction = ({
   selectedSize: string;
   selectedColor: string;
 }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const [quantity, setQuantity] = useState(1);
+
+  const handleTypeChange = (type: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(type, value);
+    router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  };
+
+  const handleQuantityChange = (type: "acc" | "decr") => {
+    if (type === "decr") {
+      setQuantity((prev) => prev + 1);
+    } else {
+      if (quantity > 1) {
+        setQuantity((prev) => prev - 1);
+      }
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4 mt-4">
       {/* size */}
@@ -21,6 +45,7 @@ const ProductInteraction = ({
             <div
               className={`cursor-pointer border p-[2px] ${selectedSize === size ? "border-gray-600" : "border-gray-300"}`}
               key={size}
+              onClick={() => handleTypeChange("size", size)}
             >
               <div
                 className={`w-6 h-6 text-center flex items-center justify-center ${selectedSize === size ? "bg-black text-white" : "bg-white text-black"}`}
@@ -32,9 +57,45 @@ const ProductInteraction = ({
         </div>
       </div>
       {/* color */}
-      <div className="flex flex-col gap-2 text-sm"></div>
+      <div className="flex flex-col gap-2 text-sm">
+        <span className="text-gray-500">Color</span>
+        <div className="flex items-center gap-2">
+          {product.colors.map((color) => (
+            <div
+              className={`cursor-pointer border p-[2px] rounded-full ${selectedColor === color ? "border-gray-600" : "border-gray-300"}`}
+              key={color}
+              onClick={() => handleTypeChange("color", color)}
+            >
+              <div
+                className={`w-6 h-6 rounded-full`}
+                style={{ backgroundColor: color }}
+              ></div>
+            </div>
+          ))}
+        </div>
+      </div>
       {/* quantity */}
-      <div className="flex flex-col gap-2 text-sm"></div>
+      <div className="flex flex-col gap-2 text-sm">
+        <span className="text-gray-500">Quantity</span>
+        <div className="flex items-center gap-2">
+          <button
+            className="cursor-pointer border border-gray-300 p-1"
+            onClick={() => handleQuantityChange("decr")}
+          >
+            <FaMinus className="w-4 h-4" />
+          </button>
+          <span>{quantity}</span>
+          <button
+            className="cursor-pointer border border-gray-300 p-1"
+            onClick={() => handleQuantityChange("acc")}
+          >
+            <FaPlus className="w-4 h-4" />
+          </button>
+        </div>
+      </div>
+      {/* buttons */}
+      <button>Add to Cart</button>
+      <button>Buy this item</button>
     </div>
   );
 };
